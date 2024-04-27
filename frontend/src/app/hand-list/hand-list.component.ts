56197e203e92145db482c1c2bfcbe6b841b2c5d3
fortzsu <67285950+fortzsu@users.noreply.github.com>
2024-04-27 12:41:47 +0200

    works

--- frontend/src/app/hand-list/hand-list.component.ts
+++ frontend/src/app/hand-list/hand-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { HandDataDto } from '../model/hand-data-dto';
 import { HandService } from '../service/hand-service.service';
 
@@ -6,17 +6,12 @@ import { HandService } from '../service/hand-service.service';
   selector: 'app-hand-list',
   templateUrl: './hand-list.component.html'
  })
-export class HandListComponent implements OnInit {
+export class HandListComponent {
 
   users: HandDataDto[] = [];
 
   constructor(private handService: HandService) {
-  }
-
-  ngOnInit() {
-    this.handService.findAll().subscribe((data:any) => {
-      this.users = data;
-    });
+      this.onSave();
   }
 
   onSave() {
